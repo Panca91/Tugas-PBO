b340ec19da4a6df6b557dbe0786a67aeab03fe8e
Panca Aulia Rahman <73013831+Panca91@users.noreply.github.com>
2022-03-09 22:55:49 +0700

    Delete tugas2 directory

--- tugas2/MenghitungBidang.java
+++
@@ -1,6 +0,0 @@
-package tugas2;
-
-public interface MenghitungBidang {
-    double luas();
-    double keliling(); 
-}
--- tugas2/MenghitungRuang.java
+++
@@ -1,6 +0,0 @@
-package tugas2;
-
-public interface MenghitungRuang {
-    double volume();
-    double luasPermukaan();
-}
